8e241f4e85626e98b0729eda5ed4d284ef64e082
Andreas Drollinger <andreas@drollinger.ch>
2016-09-11 08:59:31 +0200

    Comit of new plugin for basic authentication

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,3 +1,4 @@
 if {![package vsatisfies [package provide Tcl] 8.5]} {return}
 package ifneeded t2ws 0.5 [list source [file join $dir t2ws.tcl]]
 package ifneeded t2ws::template 0.3 [list source [file join $dir t2ws_template.tcl]]
+package ifneeded t2ws::bauth 0.1 [list source [file join $dir t2ws_bauth.tcl]]
